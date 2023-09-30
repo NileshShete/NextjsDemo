4fdea90008e2b66b201bceda67fc2154a9b58a1d
nilesh.shete <nilesh.shete@easebuzz.in>
2023-09-30 23:41:55 +0530

    Using the API Endpoints

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,7 +1,18 @@
 import * as fs from 'fs';
-export default function handler(req, res) {
-    fs.readdir("blogdata", (err, data) => {
-        console.log('data', data)
-        res.status(200).json(data)
-    })
+export default async function handler(req, res) {
+
+    let data = await fs.promises.readdir("blogdata");
+    let myfile;
+    let allBlogs = [];
+    for (let index = 0; index < data.length; index++) {
+        const item = data[index];
+        try {
+            let myfile = await fs.promises.readFile(('blogdata/' + item), 'utf-8');
+            let parsedData = JSON.parse(myfile);
+            allBlogs.push(parsedData);
+        } catch (error) {
+            console.error('Error parsing JSON file ' + item + ':', error);
+        }
+    }
+    res.status(200).json(allBlogs)
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "../src/app/ Blog.module.css"
 import Link from 'next/link'
+import "../styles/Blog.module.css"
 const Blog = () => {
+    const [blogs, setBlogs] = useState([]);
+    console.log('blogs', blogs)
+
+    useEffect(() => {
+        console.log("useeffect is running");
+        fetch('http://localhost:3000/api/blogs').then((a) => {
+            return a.json();
+        })
+            .then((parsed) => {
+                setBlogs(parsed)
+            })
+    }, [])
+
     return <div className={styles.container}>
         <main className={styles.main}>
-            <div>
-                <Link href={'/blogpost/learn-javascript'}>
-                    <h3 className={styles.blogItemh3}>How to learn JavaScript in 2022?</h3></Link>
-                <p>JavaScript is the language used to design logic for the web</p>
-            </div>
-            <div className="blogItem">
-                <h3>How to learn JavaScript in 2022?</h3>
-                <p>JavaScript is the language used to design logic for the web</p>
-            </div>
-            <div className="blogItem">
-                <h3>How to learn JavaScript in 2022?</h3>
-                <p>JavaScript is the language used to design logic for the web</p>
-            </div>
+            {blogs.map((blogitem) => {
+                return (
+                    <div key={blogitem.slug}>
+                        <Link href={`/blogpost/${blogitem.slug}`}>
+                            <h3 className={styles.blogItemh3}>{blogitem.title}</h3></Link>
+                        <p className={styles.blogItemp}>{blogitem.content.slice(0, 150)}...</p>
+                    </div>
+                )
+            })}
         </main>
     </div>
 };
