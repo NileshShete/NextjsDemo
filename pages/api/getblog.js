9f0d2e59fc9572f483732920b027589c4960da26
nilesh.shete <nilesh.shete@easebuzz.in>
2023-10-01 01:00:13 +0530

    Fetching BlogPost

--- pages/api/getblog.js
+++ pages/api/getblog.js
@@ -1,10 +1,8 @@
 // http://localhost:3000/api/getblog?slug=how-to-learn-javascript
 
-import { error } from 'console';
 import * as fs from 'fs';
 export default function handler(req, res) {
     fs.readFile(`blogdata/${req.query.slug}.json`, 'utf-8', (err, data) => {
-        console.log('req', req.query.slug)
         if(err){
             res.status(500).json({error:"Internal server error"});
         }
